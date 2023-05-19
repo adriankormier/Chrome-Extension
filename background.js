8ca7ff219829d703def7a8d256f1ef834dc44844
Adrian <adriankormier@gmail.com>
2023-05-18 17:35:41 -1000

    Palindrome Finder logs valid palindromes to the console log

--- background.js
+++ background.js
@@ -1,102 +1,70 @@
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.action.setBadgeText({
-//     text: "WTF?",
-//   });
-// });
-
-
-// function wordExtraction() {
-//   let textContent = document.body.innerText;
-//   let wordArray = textContent.split(/\s+/);
-//   console.log(wordArray)
-//   return wordArray;
-// }
-
-
-// chrome.action.onClicked.addListener((tab) => {
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: wordExtraction()
-//   }, (results) => {
-//     if (chrome.runtime.lastError || !results || !results.length) {
-//       console.log('Error injecting script or no results returned.')
-//     } else {
-//       console.log('Words from page:', results[0].result)
-//     }
-//   });
-// });
-/////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////
-// chrome.action.onClicked.addListener(async (tab) => {
-//   await chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     files: ['contentScript.js'],
-//   });
-// });
-
-
-
-
-
-// function wordExtraction() {
-//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//     chrome.scripting.executeScript({
-//       target: { tabId: tabs[0].id },
-//       function: extractWords,
-//     });
-//   });
-// }
-
-// function extractWords() {
-//   let textContent = document.body.innerText;
-//   let wordArray = textContent.split(/\s+/);
-//   console.log(wordArray);
-// }
-
-// chrome.action.onClicked.addListener(wordExtraction);
-
-
-
-////////////////CHATGPT/////////////////
 // The function to extract words from the webpage.
 function getWordsFromPage() {
+  // grab all innerText from the body element
   let textContent = document.body.innerText;
   let wordArray = textContent.split(/\s+/);
-  return wordArray;
+  console.log('textContent:' , textContent)
+  // iterate through array of words and filter out any words that contain numbers or symbols
+  let filteredText = wordArray.filter(word => {
+    // return only words that pass the regular expression (start at the beginning of word and check until the end. any words that contain numbers or symbols return falsy)
+    return word.match(/^\b[a-zA-Z]+\b$/,"");
+  });
+  // return filteredText; <--- This just returns all words found on the page
+  ////////////////////////////////////////////////////////////////
+  // this function returns the largest word found within the page
+  /*let biggestWord = filteredText.reduce((acc, curr) => {
+    if (curr.length > acc.length) acc = curr;
+    return acc;
+  }, [])
+  console.log('biggest word = ', biggestWord)
+  // console.log('filtered text:', filteredText)
+  
+  return biggestWord;*/
+  /////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////
+  // this function returns any palindromes found within the page //
+  const palindromes = (array) => {
+    let reverse = '';
+    const output = [];
+    array.forEach(el => {
+      // console.log(reverse);
+      reverse = el.split("").reverse().join("");
+      if (el === reverse && el.length >= 3) output.push(el)
+    })
+    if (!output) {
+      return 'No palindromes found!'
+    } else {
+      return output;
+    }
+  }
+  return palindromes(filteredText);
+ 
 }
 
+//add event listener that listens for message from popup.js telling it to extract words from the page
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.command === 'extractWords') {
+    //query the tabs of the current active window to create an array of tabs
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      //if tabs length is 0, no active tabs are found and message is logged to console
       if (tabs.length === 0) {
         console.log("No active tabs found.");
         return;
       }
+      // tab is set to tabs array's first element
       let tab = tabs[0];
+      // execute script that targets current tab and runs the getWordsFromPage function
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
         function: getWordsFromPage
       }, (results) => {
-        if (chrome.runtime.lastError || !results || !results.length) {
+        // if an error occurs, or no results are returned, console log the error
+        if (chrome.runtime.lastError || !results || !results.length || !results[0]) {
           console.log('Error injecting script or no results returned.');
         } else {
-          console.log('Words from page:', results[0].result);
+          // upon successfully grabbing all the words from the page, do something with them?
+          console.log('Palindromes found:', results[0].result);
+          console.log(results)
         }
       });
     });
--- popup.js
+++ popup.js
@@ -1,30 +1,4 @@
-// document.getElementById('myButt').addEventListener('click', function() {
-//   console.log('button works!')
-
-// });
-//////////////////
-
-// chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//     const tab = tabs[0];
-//     chrome.tabs.executeScript(tab.id, { file: 'content.js' }, () => {
-//       chrome.tabs.sendMessage(tab.id, { action: 'countWords' }, (response) => {
-//         const wordCountElement = document.getElementById('wordCount');
-//         wordCountElement.textContent = response.wordCount.toString();
-//       });
-//     });
-//   });
-
-
-/////////////////////////////
-
-// function extractWords() {
-//   const textContent = document.body.innerText;
-//   const wordArray = textContent.split(/\s+/);
-//   console.log(wordArray);
-// }
-
-///////////CHATGPT///////////////
-
+//add a 'click' event listener to the extract button that sends a message to the content.js file that we want to extract the words from the page
 document.getElementById('extract-button').addEventListener('click', () => {
   chrome.runtime.sendMessage({ command: 'extractWords' });
 });
